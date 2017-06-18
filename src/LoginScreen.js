e95e3fa9696f17d041cc6d038cbb37ea24a3210b
Jan Toman <toman.jan@gmail.com>
2017-06-18 12:17:52 +0200

    +) login component +) rename to MapBook

---
+++ src/LoginScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './loginscreen.css';
+
+const LoginScreen = props => (
+<div className="screen--login">
+
+    <div className="block--product-name">
+      <h1>MapBook</h1>
+      <div>Let books inspire your next travel</div>
+    </div>
+
+    <a href="#" className="button button--big">
+        Connect to Goodreads account
+    </a>
+
+</div>
+);
+
+export default LoginScreen;
